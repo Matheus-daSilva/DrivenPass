9c454cc61e0a28eb324a8ab581445c1c9615dd96
ElderGr <gbielrs@gmail.com>
2022-07-28 14:43:05 -0300

    Preparando revisão de código

--- src/schemas/notesSchema.ts
+++
@@ -1,7 +0,0 @@
-import joi from 'joi';
-
-export const notesSchema = joi.object({
-    title: joi.string().max(50).required(),
-    text: joi.string().max(1000).required(),
-    userId: joi.number()
-})
